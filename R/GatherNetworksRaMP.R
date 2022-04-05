596acfd98edbc92d981789e33a1c3e8972e2b9e3
Max McGrath <max.a.mcgrath@gmail.com>
2022-04-05 17:40:06 -0600

    Update to use RaMP-DB with local mysql server

---
+++ R/GatherNetworksRaMP.R
@@ -0,0 +1,200 @@
+#' @title Gather pathway information from RaMP-DB
+#' @name GatherNetworks
+#' @description Takes a \code{\link[SummarizedExperiment]{SummarizedExperiment}}
+#'  and constructs a list of RaMP pathways along with their associated networks
+#'  
+#' @param SE A \code{\link[SummarizedExperiment]{SummarizedExperiment}} with
+#'     the features of interest in the 
+#'     first \code{\link[SummarizedExperiment]{assay}}.
+#' @param ID Character vector indicating the name of the column(s) in \code{SE}
+#'     row data containing database IDs
+#' @param minPathwaySize Filter pathways that are below a minimum size
+#'
+#' @details
+#' Queries local RaMP database for known molecular interactions between
+#' included metabolites
+#'
+#' @return
+#' a list object containing the original SummarizedExperiment and
+#' igraph network objects
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' data(smokers)
+#' 
+#' # Create RaMP environment
+#' pkg.globals <- setConnectionToRaMP(dbname = "ramp2", username = "root", 
+#'                                    conpass = "", host = "localhost")
+#' 
+#' # Gather networks from RaMP-DB
+#' networks <- GatherNetworksRaMP(SE = smokers, ID = "kegg_id",
+#'                                minPathwaySize = 5)
+#' }
+#' 
+#' @importFrom RaMP setConnectionToRaMP getPathwayFromAnalyte rampFastCata
+#' @importFrom SummarizedExperiment assays colData rowData
+#' @importFrom tibble as_tibble column_to_rownames rownames_to_column
+#' @importFrom stats binomial complete.cases dist formula glm lm median 
+#'     model.matrix pchisq pnorm resid uniroot
+#' @importFrom igraph V graph.laplacian graph_from_adjacency_matrix 
+#'     graph_from_data_frame delete_vertices vertices gorder
+#' @importFrom data.table transpose
+#' @importFrom methods is
+#' @importFrom magrittr %>%
+#' @importClassesFrom SummarizedExperiment SummarizedExperiment
+#'
+#' @export
+GatherNetworksRaMP <- function(SE, IDs, minPathwaySize = 5) {
+    
+    # TODO: Add check for acceptable prefixes (hmdb, kegg, etc.)
+    # TODO: Figure out if RaMP has different species and adjust accordingly
+    
+    # Input validation
+    if (!is(SE, "SummarizedExperiment")) {
+        stop("SE object is not a SummarizedExperiment")
+    }
+    
+    if (as.integer(minPathwaySize) < 2) {
+        stop("Minimum pathway size should be 2 or more")
+    }
+    
+    # TODO: Update ID to IDs (make function work with multiple ID columns)
+    ID <- IDs
+    
+    # Check RaMP connection - currently not sure if necessary so commented out
+    # connectToRaMP()
+    
+    # Unpack SE object into phenotype, metabolite, and pathway data
+    mD <- assays(SE)[[1]]
+    tmD <- .transposeTibble(mD)
+    pD <-
+        tibble::as_tibble(rowData(SE), .name_repair = "minimal")
+    pD$rowname <- rownames(rowData(SE))
+    pD <- column_to_rownames(pD, var = "rowname")
+    cD <-
+        tibble::as_tibble(colData(SE), .name_repair = "minimal")
+    cD$rowname <- rownames(colData(SE))
+    cD <- column_to_rownames(cD, var = "rowname")
+    
+    # Validate Database IDs
+    if (!(ID %in% colnames(pD))) {
+        stop(ID,
+             " not found in SummarizedExperiment object rowData")
+    }
+    
+    # Remove metabolites with zero variance
+    remove_vars <- colnames(tmD)[!as.vector(unlist(lapply(tmD, function(x)
+        length(unique( x )) > 1)))]
+    
+    # Throw warning if metabolites were removed
+    if (length(remove_vars) > 0) {
+        warning(
+            "Removed the following metabolites from analysis due to
+            insufficient variance:",
+            paste(remove_vars, collapse = ", ")
+        )
+        tmD <- tmD[, !(colnames(tmD) %in% remove_vars)]
+    }
+    
+    # Compile pathway data
+    rtn <- .getNetworks(metabolites = pD[[ID]], tmD = tmD,
+                        minPathwaySize = minPathwaySize)
+    
+    rtn$SE <- SE
+    
+    rtn
+    
+}
+
+.getNetworks <- function(metabolites, tmD, minPathwaySize) {
+    
+    # Create ramp connection (environment with parent namespace:RaMP)
+    # pkg.globals <- setConnectionToRaMP(dbname = dbName, conpass = conPass)
+    
+    # Get pathways associated with analytes
+    pathwaydfids <- getPathwayFromAnalyte(metabolites)
+    
+    # Create list of all pathway names
+    pathways <- unique(pathwaydfids$pathwayName)
+    
+    # Get genes which catalyze analytes
+    new.metabolites <- rampFastCata(analytes = metabolites)
+    
+    # Get edges - defined as analytes which are both catalyzed by the same gene
+    #   and are within the same pathway
+    a <- new.metabolites[, 1:2]
+    b <- a
+    names(b) <- c("Output_Analyte", "Input_CatalyzedBy_CommonName")
+    c <- merge(a, b, all = T)
+    d <- tolower(.colwise(c[c$Input_Analyte != c$Output_Analyte, 2:3]))
+    filtered_pathways <- pathwaydfids[pathwaydfids$commonName %in% d,1:2]
+    colnames(d) <- c("Input_Analyte", "Output_Analyte")
+    # this part is slow, could be improved
+    e <- merge(d, filtered_pathways, by.x = "Input_Analyte", 
+               by.y = "commonName")
+    f <- merge(e, filtered_pathways, by.x = "Output_Analyte", 
+               by.y = "commonName")
+    g <- f[f$pathwayName.x == f$pathwayName.y,] %>% na.omit()
+    pathway_names <- unique(g$pathwayName.x)
+    
+    # Create network graphs for all pathways with at least 1 edge
+    networks <- list()
+    
+    for (pathway in pathway_names){
+        all_metabs_pathway <- pathwaydfids[pathwaydfids$pathwayName == pathway,]
+        path_network_df <- g[g$pathwayName.x == pathway,]
+        edges <- path_network_df[,1:2]
+        
+        edges <- .colwise(edges)
+        path_graph <- graph_from_data_frame(edges, directed = FALSE)
+        single_nodes <- 
+            all_metabs_pathway$commonName[!(all_metabs_pathway$commonName %in% 
+                                                path_network_df$Input_Analyte |
+                                                all_metabs_pathway$commonName %in%  # This and next two lines added, need to check correctness
+                                                path_network_df$Output_Analyte)]
+        single_nodes <- unique(single_nodes)
+        path_graph <- path_graph + vertices(single_nodes)
+        
+        # Remove edges vertices which do not appear in original data
+        rmVertices <- names(V(path_graph))[!(names(V(path_graph)) %in% 
+                                                 colnames(tmD))]
+        path_graph <- delete_vertices(path_graph, rmVertices)
+        
+        
+        networks[[pathway]] <- path_graph
+    }
+    
+    # Remove pathways with fewer vertices than minPathwaySize
+    networks <- lapply(networks, function (graph) {
+        if (gorder(graph) < minPathwaySize) {
+            rtn <- NULL
+        } else {
+            rtn <- graph
+        }
+        
+        rtn
+    })
+    
+    networks <- networks[lengths(networks) == 10]
+    
+    return(list(networks = networks))
+}
+
+# Function for getting unique pairs of metabolites from two columns
+.colwise <- function(dat) { 
+    unique(cbind(pmin(dat[,1], dat[,2]), pmax(dat[,1], dat[,2])))
+}
+
+
+# Custom function to transpose while preserving names
+.transposeTibble <- function(df) {
+    t_df <- data.table::transpose(df)
+    colnames(t_df) <- rownames(df)
+    rownames(t_df) <- colnames(df)
+    t_df <- t_df %>%
+        tibble::rownames_to_column() %>%
+        tibble::as_tibble()
+    t_df <- column_to_rownames(t_df, var = "rowname")
+    return(t_df)
+}
---
+++ R/PaIRKATRaMP.R
@@ -0,0 +1,205 @@
+#' @title Perform PaIRKAT on the output from the GatherNetworksRaMP function
+#' @name PaIRKATRaMP
+#' @description
+#'
+#' Pathway Integrated Regression-based Kernel Association Test (PaIRKAT) is a
+#' model framework for assessing statistical relationships between networks
+#' and some outcome of interest while adjusting for
+#' potential confounders and covariates.
+#'
+#' Use of PaIRKAT is motivated by the analysis of networks of metabolites
+#' from a metabolomics assay and the relationship of those networks with a
+#' phenotype or clinical outcome of interest, though the method can be
+#' generalized to other domains.
+#'
+#' @param formula.H0 The null model in the "formula" format used
+#' in \code{\link[stats]{lm}} and \code{\link[stats]{glm}} functions
+#' @param networks networks object obtained
+#' with \code{\link[pairkat]{GatherNetworks}}
+#' @param tau A parameter to control the amount of smoothing,
+#' analagous to a bandwidth parameter in kernel smoothing. We found 1 often
+#' gave reasonable results, as over-smoothing can lead to
+#' inflated Type I errors.
+#'
+#' @details The PaIRKAT method is to update the feature matrix, \eqn{Z},
+#' with the regularized normalized Laplacian, \eqn{L_R}, before performing
+#' the kernel association test. \eqn{L_R} is calculated using a "linear"
+#' regularization, \deqn{L_R = (I +\tau L)^-1,} where \eqn{I} is the identity
+#' matrix, \eqn{\tau} is a regularization parameter that controls the amount
+#' of smoothing, and \eqn{L} is the graph's normalized Laplacian. The updated
+#' feature matrix, \eqn{Z*L_R} is matrix used for the kernel association
+#' test. \cr
+#' The linear regularization and Gaussian kernel is used for all tests.
+#' See Carpenter 2021 for complete details on PaIRKAT and Smola 2003
+#' for information about graph regularization
+#'
+#' @references
+#' Carpenter CM, Zhang W, Gillenwater L, Severn C, Ghosh T, Bowler R, et al.
+#' PaIRKAT: A pathway integrated regression-based kernel association test
+#' with applications to metabolomics and COPD phenotypes.
+#' bioRxiv. 2021 Apr 26;2021.04.23.440821.
+#'
+#' Smola AJ, Kondor R. Kernels and Regularization on Graphs.
+#' In: Schölkopf B, Warmuth MK, editors. Learning Theory and Kernel Machines.
+#' Berlin, Heidelberg: Springer Berlin Heidelberg; 2003. p. 144–58.
+#' (Goos G, Hartmanis J, van Leeuwen J, editors. Lecture Notes in Computer
+#' Science; vol. 2777). http://link.springer.com/10.1007/978-3-540-45167-9_12
+#'
+#' @return
+#'
+#' a list object containing the formula call and results by pathway
+#'
+#' @examples
+#' \dontrun{
+#' data(smokers)
+#' 
+#' # Create RaMP environment
+#' pkg.globals <- setConnectionToRaMP(dbname = "ramp2", username = "root", 
+#'                                    conpass = "", host = "localhost")
+#' 
+#' # Gather networks from RaMP-DB
+#' networks <- GatherNetworksRaMP(SE = smokers, ID = "kegg_id",
+#'                                minPathwaySize = 5)
+#'                                
+#' # Run PaIRKAT Analysis
+#' output <- PaIRKAT(log_FEV1_FVC_ratio ~ age, networks = networks)
+#'
+#' # View Results
+#' output$results
+#' }
+#'
+#' @export
+#'
+PaIRKATRaMP <- function(formula.H0, networks, tau = 1) {
+    
+    # check if a valid networks object has been passed
+    if(!is(networks, "list")){
+        stop("Invalid `networks` object. Pass a valid object created with
+             GatherNetworks function")
+    }
+    if(!all(c("networks","pdat","SE") %in% names(networks))){
+        stop("Invalid `networks` object. Pass a valid object created with
+             GatherNetworks function")
+    }
+    if(length(networks$networks) == 0){
+        stop("`networks` object contains 0 pathways")
+    }
+    
+    # Unpack SE object into phenotype, metabolite, and pathway data
+    SE <- networks$SE
+    mD <- assays(SE)[[1]]
+    tmD <- .transposeTibble(mD)
+    pD <- tibble::as_tibble(rowData(SE), .name_repair = "minimal")
+    pD$rowname <- rownames(rowData(SE))
+    pD <- tibble::column_to_rownames(pD, var = "rowname")
+    cD <- tibble::as_tibble(colData(SE), .name_repair = "minimal")
+    cD$rowname <- rownames(colData(SE))
+    cD <- tibble::column_to_rownames(cD, var = "rowname")
+    
+    # check formula.H0 for proper formatting
+    if (!is(formula.H0, "formula")){
+        stop("Invalid `formula.H0`. Please format
+             as `outcome ~ covariate_1 + covariate_2 + ... covariate_n`")
+    }
+    
+    # parse variables from formula
+    keepVars <- paste(formula.H0[[2]])
+    if (length(formula.H0[[3]]) > 1) {
+        for (i in seq_len(length(formula.H0[[3]]))) {
+            keepVars <- c(keepVars, paste(formula.H0[[3]][[i]]))
+        }
+    }
+    else {
+        keepVars <- c(keepVars, paste(formula.H0[[3]]))
+    }
+    
+    # remove + from list of variables
+    keepVars <- keepVars[keepVars != "+"]
+    
+    # check if variables are in the data
+    if(!all(keepVars %in% names(cD))){
+        stop(keepVars[!(keepVars %in% names(cD))]," not found in data")
+    }
+    
+    cD <- cD[, names(cD) %in% keepVars]
+    
+    # check for missing data and subset complete cases
+    completeCases <- complete.cases(cD)
+    cD <- cD[completeCases,]
+    tmD <- tmD[completeCases,]
+    
+    # check properties of outcome and format or throw appropriate errors
+    outcome_vector <- cD[[keepVars[1]]]
+    
+    if (length(unique(outcome_vector)) == 2) {
+        out.type <- "binary"
+        message("Binary outcome detected.
+                Null model will be a glm with logit link")
+    } else if (length(unique(outcome_vector)) > 2) {
+        out.type <- "continuous"
+        message("Continuous outcome detected.
+                Null model will be a linear model.")
+    }
+    
+    if (!is.numeric(outcome_vector)) {
+        if (out.type == "binary") {
+            ref_level <- unique(outcome_vector)[1]
+            pos_level <- unique(outcome_vector)[2]
+            outcome_vector[outcome_vector == ref_level] <- 0
+            outcome_vector[outcome_vector == pos_level] <- 1
+            cD[keepVars[1]] <- outcome_vector
+            
+            warning(
+                "Binary outcome is non-numeric, encoding",
+                pos_level,
+                "as 1 and",
+                ref_level,
+                "as 0."
+            )
+        }
+        else{
+            stop("Invalid formula: outcome is non-numeric")
+        }
+        
+    }
+    
+    # Perform kernel tests
+    pp_frame <- NULL
+    
+    for (i in seq_len(length(networks$networks))) {
+        G <- networks$networks[[i]]
+        # varnames <- igraph::V(G)$label
+        varnames <- names(igraph::V(G))
+        ZZ <- scale(tmD[, varnames[varnames %in% colnames(tmD)]])
+        
+        ## normalized Laplacian
+        L <- igraph::graph.laplacian(G, normalized = TRUE)
+        rho <- median(dist(ZZ))
+        
+        tryCatch(Z <- ZZ %*% solve(diag(nrow(L)) + tau * L),
+                 error = function(condition) {
+                     message(paste0("Error with graph ", i))
+                     message("Original error")
+                     message(condition)
+                 })
+        
+        K <- Gaussian_kernel(rho, Z)
+        
+        if (out.type == "continuous") {
+            pp <- SKAT.c(formula.H0, .data = cD, K = K)
+        }
+        
+        if (out.type == "binary") {
+            pp <- SKAT.b(formula.H0, .data = cD, K = K)
+        }
+        
+        pp$pathway <- names(networks$networks[i])
+        pp_frame <- rbind(pp_frame, as.data.frame(pp))
+        
+        
+    }
+    list(call = formula.H0, results = pp_frame[, c(3, 2, 1)])
+}
+
+
+    
